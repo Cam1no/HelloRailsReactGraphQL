dab87b4bf92c97505251d46e570002a044e55eef
kinjo yuji <kinjoyuji@gmail.com>
2018-05-12 03:28:13 +0900

    HelloBootstrap

--- app/javascript/src/app/App.jsx
+++ app/javascript/src/app/App.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Alert, Button } from 'react-bootstrap';
 
-export const HellWorld = () => {
+export const HelloWorld = () => {
   return (
-    <div>
-      HelloWorld
-    </div>
+    <Alert bsStyle="danger" onDismiss={() => null}>
+      <h4>Oh snap! You got an error!</h4>
+      <p>
+        Change this and that and try again. Duis mollis, est non commodo
+        luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+        Cras mattis consectetur purus sit amet fermentum.
+      </p>
+      <p>
+        <Button bsStyle="danger">Take this action</Button>
+        <span> or </span>
+        <Button onClick={() => null}>Hide Alert</Button>
+      </p>
+    </Alert>
   )
 }
 
@@ -14,12 +25,11 @@ class App extends Component {
     super(props);
   }
 
-
   render() {
     return (
       <div className="app-container">
         <Switch>
-          <Route path="/" component={HellWorld} />
+          <Route path="/" component={HelloWorld} />
         </Switch>
       </div>
     );
